d4ea3d76b65b163fdde4120dd9a90c2ca798fe0d
vika L <vika.lapsar@gmail.com>
2015-04-01 21:22:56 +0300

    2 sketches for changing HC Bluetooth Module settings

---
+++ sketches/setBTsettingsFromSetup.ino
@@ -0,0 +1,41 @@
+#include <SoftwareSerial.h>
+#define RX_PIN 10
+#define TX_PIN 11
+
+SoftwareSerial btSerial = SoftwareSerial(RX_PIN, TX_PIN);
+
+void setup() {
+
+  pinMode(TX_PIN, OUTPUT);
+  pinMode(RX_PIN, INPUT);
+
+  btSerial.begin(9600);
+  Serial.begin(9600);
+
+  Serial.println("Bluetooth with 9600");
+  btSerial.write("AT");
+  delay(1100);
+  while (btSerial.available())
+    Serial.write(btSerial.read());
+
+  Serial.println("\nChange name");
+  btSerial.write("AT+NAME");
+  delay(1100);
+  while (btSerial.available())
+    Serial.write(btSerial.read());
+
+  Serial.println("\nAjusting PIN - 6666");
+  btSerial.write("AT+PIN6666");
+  delay(1100);
+  while (btSerial.available())
+    Serial.write(btSerial.read());
+ 
+  Serial.println("\nBluetooth version:");
+  btSerial.write("AT+VERSION");
+  delay(1100);
+  while (btSerial.available())
+    Serial.write(btSerial.read());
+}
+
+void loop() {
+}
